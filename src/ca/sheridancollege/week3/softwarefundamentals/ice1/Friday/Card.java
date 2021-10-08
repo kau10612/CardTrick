edae81a41de050aefec116f576fb5ea716099451
kau10612 <91230342+kau10612@users.noreply.github.com>
2021-10-08 02:06:37 -0400

    Update Card.java
    demo on fetch and merge

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/Card.java
@@ -11,6 +11,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
  * @modifier Ramneek Kaur 
  * Student Id: 991631069
  */
+//changes done in fetch and merge
 public class Card {
 
     private String suit; //clubs, spades, diamonds, hearts
